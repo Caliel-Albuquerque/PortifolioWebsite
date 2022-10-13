9dd79fff038b176c3346402df6e04ca4db6a5632
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2022-10-13 15:20:35 -0300

    NewChanges

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,25 +41,7 @@ for(i = 0; i < accordionContent.length; i++){
 
 
 /*==================== QUALIFICATION TABS ====================*/
-/*const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
 
-
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target)
-
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('qualification_active')
-        })
-        target.classList.add('qualification_active')
-
-        tab.forEach(tab => {
-            tab.classList.remove('qualification_active')
-        })
-        tab.classList.add('qualification_active')
-    })
-})*/
 
 var qualificationExp = document.getElementById("work");
 var qualificationEdu = document.getElementById("education");
