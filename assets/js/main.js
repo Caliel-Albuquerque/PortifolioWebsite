57ae0fd5d980e2ccdd3e8f34111437622ac4d547
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2022-10-14 13:46:46 -0300

    newStyle

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,10 +68,20 @@ qualificationEdu.addEventListener('click', function(){
 })
 
 
-/*==================== SERVICES MODAL ====================*/
-
-
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portifolio_container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   
+  });
 
 
 /*==================== TESTIMONIAL ====================*/
