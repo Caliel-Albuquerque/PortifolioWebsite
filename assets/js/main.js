8b4a842af58a0d78f4470b1ba0ef043a762d84e5
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2022-10-15 11:29:10 -0300

    newStyles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 /*==================== MENU SHOW E HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
@@ -13,7 +13,7 @@ if(navToggle){
 
 /*===== MENU HIDDEN =====*/
 
-if(navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
@@ -22,7 +22,7 @@ if(navClose){
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav_link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
@@ -32,10 +32,10 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 var accordionContent = document.getElementsByClassName('skills_header');
 
-for(i = 0; i < accordionContent.length; i++){
-    accordionContent[i].addEventListener('click', function() {
-       accordionContent[i].classList.toggle("active");
-        
+for (i = 0; i < accordionContent.length; i++) {
+    accordionContent[i].addEventListener('click', function () {
+        accordionContent[i].classList.toggle("active");
+
     });
 }
 
@@ -49,18 +49,18 @@ var qualificationEdu = document.getElementById("education");
 var qualificationEduContent = document.getElementById("contentEdu");
 var qualificationExpContent = document.getElementById("contentExp");
 
-if(qualificationExpContent.style.display = "block"){
+if (qualificationExpContent.style.display = "block") {
     qualificationExpContent.style.display = "none"
 }
 
-qualificationExp.addEventListener('click', function(){
+qualificationExp.addEventListener('click', function () {
     qualificationExpContent.style.display = "block"
     qualificationEduContent.style.display = "none"
     qualificationExp.classList.add("qualification_active")
     qualificationEdu.classList.remove("qualification_active")
 })
 
-qualificationEdu.addEventListener('click', function(){
+qualificationEdu.addEventListener('click', function () {
     qualificationExpContent.style.display = "none"
     qualificationEduContent.style.display = "block"
     qualificationEdu.classList.add("qualification_active")
@@ -73,15 +73,15 @@ let swiper = new Swiper(".portifolio_container", {
     cssMode: true,
     loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-   
-  });
+
+});
 
 
 /*==================== TESTIMONIAL ====================*/
@@ -90,10 +90,45 @@ let swiper = new Swiper(".portifolio_container", {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
